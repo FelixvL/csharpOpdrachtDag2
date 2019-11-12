06378fedb25d3e1c1f81eafe5ab2aab0260e0ef2
FelixvL <digitaleservice@gmail.com>
2019-11-12 09:04:46 +0100

    where werkt

---
+++ OpdrachtASPCoreDag2/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OpdrachtASPCoreDag2.Models;
+
+namespace OpdrachtASPCoreDag2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+//            ProbeerselASPDBEntities dbcontext = new ProbeerselASPDBEntities();
+            OpdrachtASPCoreDag2.Opdracht1coredag2Context c = new OpdrachtASPCoreDag2.Opdracht1coredag2Context();
+            //c.Teacher.SelectMany<Teacher>(a=> { new Teacher(); }).ToList();
+            Teacher jojo = c.Teacher.Find(1);
+            Teacher g = new Teacher();
+            g.AddedOn = new DateTime();
+            g.Name = "Fred";
+            g.Skills = "nope";
+            g.Salary = 5;
+            g.TotalStudents = 14;
+            c.Teacher.Add(g);
+            c.SaveChanges();
+            var ab = c.Teacher.Where(a => a.Name == "Fred").ToList();
+            return View(ab);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OpdrachtASPCoreDag2/Opdracht1coredag2Context.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace OpdrachtASPCoreDag2
+{
+    public partial class Opdracht1coredag2Context : DbContext
+    {
+        public Opdracht1coredag2Context()
+        {
+        }
+
+        public Opdracht1coredag2Context(DbContextOptions<Opdracht1coredag2Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Teacher> Teacher { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-LS3KMQK;Initial Catalog=Opdracht1CoreDag2;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+
+            modelBuilder.Entity<Teacher>(entity =>
+            {
+                entity.Property(e => e.AddedOn)
+                    .HasColumnType("date")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salary).HasColumnType("money");
+
+                entity.Property(e => e.Skills)
+                    .IsRequired()
+                    .HasMaxLength(250)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
